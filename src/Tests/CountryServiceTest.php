9aed15f96961f156157f55fa747be0e9314a603e
Wassim Zr <wzrigui@gmail.com>
2019-07-03 13:00:44 +0200

    fix indentation

--- src/Tests/CountryServiceTest.php
+++ src/Tests/CountryServiceTest.php
@@ -6,10 +6,10 @@ use App\CountryService as CountryService;
 
 class CountryServiceTest extends TestCase 
 {
-	protected $countryService;
-	protected $client;
+    protected $countryService;
+    protected $client;
 
-	public function setUp()
+    public function setUp()
     {
     	$this->client =  $this->getMockBuilder('App\HttpRequest')
             ->disableOriginalConstructor()
@@ -18,20 +18,17 @@ class CountryServiceTest extends TestCase
  		->method('getInfo')
      	->will($this->returnValue('200'));
 
-         $this->client->expects($this->exactly(2))
+        $this->client->expects($this->exactly(2))
          ->method('execute')
          ->willReturnOnConsecutiveCalls('[{"name":"Germany","region":"Europe","subregion":"Western Europe","population":81770900,"languages":[{"iso639_1":"de","iso639_2":"deu","name":"German","nativeName":"Deutsch"}]}]', '[{"name":"Switzerland"},{"name":"Austria"}]');
 
-          $this->countryService = new CountryService(['', 'Germany', 'Switzerland'], $this->client);
+        $this->countryService = new CountryService(['', 'Germany', 'Switzerland'], $this->client);
     }
 
     public function testGetResult()
     {
-
     	$result = $this->countryService->getResult();
 
- 		$this->assertContains("do speak the same language", $result);
+ 	$this->assertContains("do speak the same language", $result);
     }
-
-
 }
