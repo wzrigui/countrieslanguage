8147237f2b697c69f4415005e673acc68c5e9447
Wassim Zr <wzrigui@gmail.com>
2019-07-03 13:19:29 +0200

    code order

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php 
+require_once __DIR__ . '/vendor/autoload.php';
 
 use App\CountryService as CountryService;
 use App\CurlRequest as CurlRequest;
-require_once __DIR__ . '/vendor/autoload.php';
-
 
 $countriesService = new CountryService($argv, new CurlRequest());
-
 
 print_r($countriesService->getResult());
